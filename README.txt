1aaa8a9e8c107c057475385eba8a592ef2769a2a
TLink666 <wangyixin99069@163.com>
2026-06-14 10:45:40 +0800

    initial local project

---
+++ README.txt
@@ -0,0 +1,9 @@
+# Local RAG Demo
+
+A simple local RAG project using:
+
+- Ollama
+- Sentence Transformers
+- FAISS
+
+Author: Yixin Wang
---
+++ src/chunking.py
@@ -0,0 +1,13 @@
+def chunk_text(text, chunk_size=200, overlap=50):
+    chunks = []
+    
+    start = 0
+    while start < len(text):
+        end = start + chunk_size
+        chunk = text[start:end]
+        
+        chunks.append(chunk)
+        
+        start = end - overlap  # overlap避免语义断裂
+    
+    return chunks
