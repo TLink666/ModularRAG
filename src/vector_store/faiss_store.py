18427c39522262d5ac8a07e3fec6f650ef10ef4e
TLink666 <wangyixin99069@163.com>
2026-06-16 09:23:04 +0800

    storage, query list

--- src/vector_store/faiss_store.py
+++ src/vector_store/faiss_store.py
@@ -8,3 +8,12 @@ def build_index(model, docs):
     index = faiss.IndexFlatIP(embeddings.shape[1])
     index.add(embeddings)
     return index
+
+def save_index(index, docs, folder):
+    faiss.write_index(index, f"{folder}/index.faiss")
+    np.save(f"{folder}/docs.npy", docs, allow_pickle=True)
+
+def load_index(folder):
+    index = faiss.read_index(f"{folder}/index.faiss")
+    docs = np.load(f"{folder}/docs.npy", allow_pickle=True).tolist()
+    return index, docs
